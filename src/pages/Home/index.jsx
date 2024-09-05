c2868ad6cbef7efb0cd4619590dee1f73e9d83fc
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-05 15:52:06 -0300

    feat: Card components integrated with DB

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,8 +9,24 @@ import {
 import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/contacts")
+      .then(async (response) => {
+        const json = await response.json();
+        setContacts(json);
+      })
+      .catch((error) => {
+        console.log("erro", error);
+      });
+  }, []);
+
+  console.log(contacts);
+
   return (
     <>
       <Container>
@@ -20,7 +36,10 @@ export default function Home() {
           <input type="text" placeholder="Pesquise um contato..." />
         </InputSearchContainer>
         <Header>
-          <strong>3 contatos</strong>
+          <strong>
+            {contacts.length}
+            {contacts.length === 1 ? " contato" : " contatos"}
+          </strong>
           <Link to="/new">Novo contato</Link>
         </Header>
       </Container>
@@ -33,35 +52,31 @@ export default function Home() {
           </button>
         </header>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Rafael Almeida</strong>
-              <small>alimentos</small>
+        {contacts.map((contact) => (
+          <Card key={contact.id}>
+            <div className="info">
+              <div className="contact-name">
+                <strong>{contact.name}</strong>
+                {contact.category_name && (
+                  <small>{contact.category_name}</small>
+                )}
+              </div>
+              <span>{contact.email}</span>
+              <span>{contact.phone}</span>
             </div>
-            <span>rafael@alimentos.com</span>
-            <span>(48) 99999-9999</span>
-          </div>
 
-          <div className="actions">
-            <Link to="/edit/123">
-              <img src={edit} alt="Editar" height="24px" />
-            </Link>
+            <div className="actions">
+              <Link to={`/edit/${contact.id}`}>
+                <img src={edit} alt="Editar" height="24px" />
+              </Link>
 
-            <button type="button">
-              <img src={trash} alt="Deletar" height="24px" />
-            </button>
-          </div>
-        </Card>
+              <button type="button">
+                <img src={trash} alt="Deletar" height="24px" />
+              </button>
+            </div>
+          </Card>
+        ))}
       </ListContainer>
     </>
   );
 }
-
-// fetch('http://localhost:3001/categories')
-// .then((response) => {
-//   console.log('response', response)
-// })
-// .catch((error) => {
-//   console.log('erro', error)
-// })
