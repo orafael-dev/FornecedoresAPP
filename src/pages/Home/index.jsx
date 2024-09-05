59dead2453d11ec02c10987a0404a049c77c3db1
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-05 18:08:35 -0300

    feat: Added Search engine

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,19 +3,29 @@ import {
   Card,
   Container,
   Header,
-  ListContainer,
   InputSearchContainer,
+  ListHeader,
 } from "./styles";
 import arrow from "../../assets/images/icons/arrow.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/trash.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState("asc");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredContacts = useMemo(
+    () =>
+      contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    [contacts, searchTerm]
+  );
 
   useEffect(() => {
-    fetch("http://localhost:3001/contacts")
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
@@ -23,60 +33,66 @@ export default function Home() {
       .catch((error) => {
         console.log("erro", error);
       });
-  }, []);
+  }, [orderBy]);
 
-  console.log(contacts);
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
+  }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
 
   return (
-    <>
-      <Container>
-        {/* <Loader /> */}
-        {/* <Modal danger /> */}
-        <InputSearchContainer>
-          <input type="text" placeholder="Pesquise um contato..." />
-        </InputSearchContainer>
-        <Header>
-          <strong>
-            {contacts.length}
-            {contacts.length === 1 ? " contato" : " contatos"}
-          </strong>
-          <Link to="/new">Novo contato</Link>
-        </Header>
-      </Container>
+    <Container>
+      <InputSearchContainer>
+        <input
+          value={searchTerm}
+          type="text"
+          placeholder="Pesquise um contato..."
+          onChange={handleChangeSearchTerm}
+        />
+      </InputSearchContainer>
 
-      <ListContainer>
-        <header>
-          <button type="button" className="sort-button">
+      <Header>
+        <strong>
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? " contato" : " contatos"}
+        </strong>
+        <Link to="/new">Novo contato</Link>
+      </Header>
+
+      {filteredContacts.length > 0 && (
+        <ListHeader order={orderBy}>
+          <button onClick={handleToggleOrderBy} type="button">
             <span>Nome</span>
             <img src={arrow} alt="arrow" />
           </button>
-        </header>
+        </ListHeader>
+      )}
 
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                {contact.category_name && (
-                  <small>{contact.category_name}</small>
-                )}
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && <small>{contact.category_name}</small>}
             </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={edit} alt="Editar" height="24px" />
-              </Link>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="Editar" height="24px" />
+            </Link>
 
-              <button type="button">
-                <img src={trash} alt="Deletar" height="24px" />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
-    </>
+            <button type="button">
+              <img src={trash} alt="Deletar" height="24px" />
+            </button>
+          </div>
+        </Card>
+      ))}
+    </Container>
   );
 }
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -47,24 +47,27 @@ export const Header = styled.header`
     }
   }
 `;
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
+  margin-bottom: 8px;
 
-  header {
-    margin-bottom: 8px;
+  button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
 
-    .sort-button {
-      background: transparent;
-      border: none;
-      display: flex;
-      align-items: center;
-
-      span {
-        margin-right: 8px;
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main};
-      }
+    span {
+      margin-right: 8px;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
     }
+
+    img {
+      transform: ${({ order }) => order === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)'};
+      transition: transform 0.2s ease-in;
+    }
+
   }
 `;
 export const Card = styled.div`
