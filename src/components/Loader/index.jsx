37513aa4d5c336a30cc363a7a84bc5f86966655e
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-10 22:28:19 -0300

    refactor: Improved React Portal to create DOM node by javascript instead html directly

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,21 +1,22 @@
+import PropTypes from "prop-types";
 import { Overlay } from "./styles";
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 import Spinner from "../Spinner";
+import ReactPortal from "../ReactPortal";
 
 export default function Loader({ isLoading }) {
   if (!isLoading) {
-    return null
+    return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Spinner size={90}/>
-    </Overlay>,
-    document.getElementById('loader-root')
-  )
+  return (
+    <ReactPortal containerId="loader-root">
+      <Overlay>
+        <Spinner size={90} />
+      </Overlay>
+    </ReactPortal>
+  );
 }
 
 Loader.propTypes = {
-  isLoading: PropTypes.bool.isRequired
-}
+  isLoading: PropTypes.bool.isRequired,
+};
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
-import ReactDOM from "react-dom";
 import { Container, Overlay, Footer } from "./styles";
 import Button from "../Button";
+import ReactPortal from "../ReactPortal";
 
 export default function Modal({
   danger = false,
@@ -14,38 +14,47 @@ export default function Modal({
   onCancel,
   onConfirm,
 }) {
-
   if (!visible) {
-    return null
+    return null;
   }
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
-        <div className="modal-body">{children}</div>
 
-        <Footer>
-          <button
-          type="button"
-          className="cancel-button"
-          onClick={onCancel}
-          disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
+  let container = document.getElementById("modal-root");
 
-          <Button
-          type="button"
-          danger={danger}
-          onClick={onConfirm}
-          isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById("modal-root")
+  if (!container) {
+    const container = document.createElement("div");
+    container.setAttribute("id", "modal-root");
+    document.body.appendChild(container);
+  }
+
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
+          <div className="modal-body">{children}</div>
+
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
+
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
